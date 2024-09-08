b3df7beafeadd6e0bf3476721a32d13fb38791a7
razandakkak <razan_dakak@outlook.com>
2024-09-08 10:02:31 +0300

    Added a gender model testing script

---
+++ TestGender.py
@@ -0,0 +1,41 @@
+# pip install ultralytics pillow matplotlib
+# pip install opencv-python
+
+from ultralytics import YOLO
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# Path to your locally saved model (change to your local model path)
+model = YOLO('GenderModel_YOLOv10.pt')
+
+# Load and open the image from a local path
+img_path = input("Please enter the path to the image: ")
+try:
+    image = Image.open(img_path)
+except FileNotFoundError:
+    print("Image not found. Please check the path and try again.")
+    exit()
+
+# Perform prediction
+results = model.predict(image, verbose=False) #  suppresses the detailed logging
+
+# Set confidence threshold
+confidence_threshold = 0.5
+prediction_made = False
+# Iterate through results
+for result in results:
+    for box in result.boxes:
+        class_id = int(box.cls)
+        confidence = box.conf.item()
+
+        # Output based on confidence threshold
+        if confidence >= confidence_threshold:
+            prediction_made = True  # A valid prediction is made
+            if class_id == 1:  # Female with confidence > 0.5
+                print('female')
+            elif class_id == 0:  # Male with confidence > 0.5
+                print('male')
+
+# If no prediction was made with sufficient confidence
+if not prediction_made:
+    print("No confident prediction found. Please upload another image.")
